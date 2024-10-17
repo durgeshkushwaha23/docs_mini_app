f775ccd719525d00219ba2ffd6b2df4863751761
durgeshkushwaha23 <durgeshkushwaha415@gmail.com>
2024-10-17 21:42:35 +0530

    completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
+import React from "react";
 import { FaFileAlt } from "react-icons/fa";
 import { MdDownloading } from "react-icons/md";
 import { IoMdCloseCircle } from "react-icons/io";
 
-const Card = ({ data }) => {
+import { motion } from "framer-motion";
+
+const Card = ({ data, reference }) => {
   return (
     <>
-      <div className="ml-3 mt-2 card relative p-3 h-72 w-52 bg-zinc-500 rounded-md text-white">
+      <motion.div
+        drag
+        dragConstraints={reference}
+        dragTransition={{bounceDamping:100, bounceStiffness:100}}
+        whileDrag={{ scale: 1.1 }}
+        className="ml-3 mt-2 card relative p-3 h-72 w-52 bg-white rounded-md text-white"
+      >
         <FaFileAlt />
-        <p className='mt-4 leading-tight'>{data.decs}</p>
+        <p className="mt-4 leading-tight">{data.decs}</p>
         <div className="footer rounded-md absolute bottom-0 w-full py-3 px-8 left-0">
           <div className="flex gap-2 items-center justify-between mb-4">
-            <h5 className='text-xl'>{data.filesize}</h5>
-            {data.close ? <IoMdCloseCircle size={30} /> : <MdDownloading size={30} />}
+            <h5 className="text-xl">{data.filesize}</h5>
+            {data.close ? (
+              <IoMdCloseCircle size={30} />
+            ) : (
+              <MdDownloading size={30} />
+            )}
           </div>
           {data.tag.isOpen && (
             <div
               className="font-bold ml-3 text-[1.2vw]"
-              style={{ color: data.tag.textcolor }}  
+              style={{ color: data.tag.textcolor }}
             >
               {data.tag.tagTitle}
             </div>
           )}
         </div>
-      </div>
+      </motion.div>
     </>
-  )
-}
+  );
+};
 
 export default Card;
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Card from "./Card";
+import { useRef } from "react";
 
 const Forground = () => {
+
+  const ref = useRef(null);
+
   const data = [
     {
       decs: "lorem3 slflf sfnlslna fk fk  fkwnfwn kn fknwff  knfn nwfnwnfnf fn kwnf",
       filesize: "0.3mb",
       close: true,
-      tag: { isOpen: true, tagTitle: "Download now", textcolor: "red" }
+      tag: { isOpen: true, tagTitle: "Download now", textcolor: "pink" }
     },
     {
       decs: "lorem3 slflf sfnlslna fk fk  fkwnfwn kn fknwff  knfn nwfnwnfnf fn kwnf",
@@ -15,19 +19,14 @@ const Forground = () => {
       close: false,
       tag: { isOpen: true, tagTitle: "Download ", textcolor: "green" }
     },
-    {
-      decs: "lorem3 slflf sfnlslna fk fk  fkwnfwn kn fknwff  knfn nwfnwnfnf fn kwnf",
-      filesize: "0.3mb",
-      close: true,
-      tag: { isOpen: true, tagTitle: "Download now", textcolor: "blue" }
-    }
+ 
   ];
 
   return (
     <>
-      <div className="h-full flex gap-2 w-full fixed top-0 left-0 ">
+      <div  ref={ref} className="h-full flex gap-2 w-full fixed top-0 left-0 ">
         {data.map((item, index) => (
-          <Card key={index} data={item} />
+          <Card key={index} data={item} reference={ref} />
         ))}
       </div>
     </>
